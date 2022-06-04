d74d4fd5fe9cc647d99893d76b5dadb0553a9cc1
seyeogunsina <adeseyeogunsina@gmail.com>
2022-06-04 22:10:57 +0100

    update shows create route

--- app.py
+++ app.py
@@ -467,9 +467,22 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
+  form = ShowForm()
+  show = Show()
+  try:
+    show.artist_id = form.artist_id.data
+    show.venue_id = form.venue_id.data
+    show.start_time = form.start_time.data
+    db.session.add(show)
+    db.session.commit()
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
+    # on successful db insert, flash success
+    flash('Show was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('Show could not be listed due to an error')
+  finally:
+    db.session.close()
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
